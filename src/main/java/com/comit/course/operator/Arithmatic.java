1d43a96451275767ef215e4a4d5e2b632045456f
lihua <mrlu0208@gmail.com>
2023-02-14 19:54:15 -0600

    add some java class

---
+++ src/main/java/com/comit/course/operator/Arithmatic.java
@@ -0,0 +1,28 @@
+package com.comit.course.operator;
+
+class Arithmatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=8;
+		int b=3;
+	     int c=a/b;//dfdf
+		System.out.println("a/b="+ c);
+		
+		boolean equalto    = a == b;
+		boolean notequal   = a != b;
+		boolean greater    = a > b;
+		boolean lessthan   = a < b;
+		
+		int a1=++a;
+		System.out.println(a1);
+		/*
+		 * sfs
+		 * sdfsfs
+		 * sf
+		 * */
+		
+	}
+
+}
---
+++ src/main/java/com/comit/course/operator/qqq.java
@@ -0,0 +1,10 @@
+package com.comit.course.operator;
+
+public class qqq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//llll
+	}
+
+}
